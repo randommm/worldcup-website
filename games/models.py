1850d679a5b7a89b0782d755fff566c6e2dcf76c
Marco Inacio <m@marcoinacio.com>
2017-11-13 18:32:39 -0200

    substituing users for django auth backend

--- games/models.py
+++ games/models.py
@@ -1,12 +1,5 @@
 from django.db import models
-
-class User(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200, unique=True)
-    password = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.name
+from django.contrib.auth.models import User
 
 class Team(models.Model):
     name = models.CharField(max_length=200, unique=True)

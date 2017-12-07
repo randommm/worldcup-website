b213dfa2156e42169d3a816b249fa47d559e45c7
Marco Inacio <m@marcoinacio.com>
2017-12-07 20:29:50 -0200

    added arquivos autoria diniz

--- games/models.py
+++ games/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class Team(models.Model):
     name = models.CharField(max_length=200, unique=True)
+    flag = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
         return self.name

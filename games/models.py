9fe8b663252cddc9cbb661be507b81b108bdd719
Marco Inacio <m@marcoinacio.com>
2017-12-06 22:33:15 -0200

    update

--- games/models.py
+++ games/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-User._meta.get_field('email')._unique = True
+#User._meta.get_field('email')._unique = True
 
 class Team(models.Model):
     name = models.CharField(max_length=200, unique=True)
